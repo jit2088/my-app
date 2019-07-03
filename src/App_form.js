1c8b86e45d26df69dc998fc3373087a5f2dab008
jin <jin.2088@usask.ca>
2019-07-02 20:03:31 -0600

    app form

---
+++ src/App_form.js
@@ -0,0 +1,46 @@
+import React from "react"
+
+
+class App extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            firstName: "",
+            lastName: ""
+
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+            
+        })
+    }
+
+    render(){
+        return(
+            <form>
+                <input 
+                    type="text" 
+                    value = {this.state.firstName} 
+                    name = "firstName" 
+                    placeholder="First Name" 
+                    onChange={this.handleChange}
+                />
+                <br/>
+                <input 
+                    type="text" v
+                    alue = {this.state.lastName} 
+                    name="lastName" 
+                    placeholder="Second Name" 
+                    onChange={this.handleChange}
+                />
+                <h1>{this.state.firstName} {this.state.lastName}</h1> 
+                
+            </form>
+        )
+    }
+}
+
+export default App
